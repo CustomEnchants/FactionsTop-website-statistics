483bf6960f3753ed01ac910700292cb5e0027fd0
CustomEnchants <team@customenchantsdevelopment.ovh>
2024-01-08 20:39:27 +0100

    Update index

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $result = $conn->query($sql);
         <div class="container-fluid">
 			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 				<div class="navbar-nav">
-					<h3 class="nav-link text-white">CustomEnchantsDevelopment</h3>
+					<h3 class="nav-link text-white">Your Server Name</h3>
 					<a class="nav-link text-white" href="#"><h3>Home</h3></a>
 					<a class="nav-link text-white" href="#yourdiscordlink"><h3>Discord</h3></a>
 				</div>
@@ -80,22 +80,13 @@ $result = $conn->query($sql);
         </div>
       </div>
 	    <div class="card-footer">
-        <h3>Statistics from play.yourserver.com</h3>
+        <h3>Statistics from play.YourServer.com</h3>
       </div>
       </div>
 	  </div>
       </div>
     </div>
  
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
   </body>
 </html>
